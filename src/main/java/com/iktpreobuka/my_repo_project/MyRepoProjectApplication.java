f6571c9084b79957a3843f75e1a897a82be01441
Vladimir Nikolov <vladimir.nikolov.brains22@gmail.com>
2023-02-15 15:22:43 +0100

    Moj prvi commit

---
+++ src/main/java/com/iktpreobuka/my_repo_project/MyRepoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.my_repo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRepoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRepoProjectApplication.class, args);
+	}
+
+}
